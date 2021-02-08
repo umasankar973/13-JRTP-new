e2aa331d86e597ee3f9722862bcbf7c76d2e93b6
umasankar973 <63362047+umasankar973@users.noreply.github.com>
2021-02-08 19:02:13 +0530

    Update RestProj5GetMappingApplication.java
    edited

--- src/main/java/in/ashokit/RestProj5GetMappingApplication.java
+++ src/main/java/in/ashokit/RestProj5GetMappingApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestProj5GetMappingApplication {
 	
-	private RestProj5GetMappingApplication() {
-		
-	}
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestProj5GetMappingApplication.class, args);
